70cc7d5e853f3212ceb2a29839e72c359fb4b27c
patorras <triotank@gmail.com>
2022-01-15 08:58:24 +0000

    finished

---
+++ ruby/substrings/substrings.rb
@@ -0,0 +1,47 @@
+# maybe convert word to a array, then compare if word exists in dictionary, filling a hash
+
+
+dictionary = ["below", "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+
+
+def substrings(word, array)
+
+    hash = {}
+
+    # converts string to a array
+    word = word.split(" ")
+
+
+
+    
+    # use include? like array[0].include?(word)
+    word.each do |letter|
+        array.each do |string|
+            if letter.downcase.include?(string) 
+
+                if hash[string] == nil 
+                    hash[string] = 1
+                elsif hash[string] > 0
+                    hash[string] += 1
+                end
+
+                
+
+            
+            end
+
+            
+        end
+    end
+    
+
+    puts hash.sort.to_h
+    
+
+
+
+end
+
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
