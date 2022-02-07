33b5bb3670979351385be248e4969d0083ef55c8
patorras <triotank@gmail.com>
2022-02-07 18:01:50 +0000

    add board

--- ruby/tic_tac_toe/main.rb
+++ ruby/tic_tac_toe/main.rb
@@ -3,8 +3,16 @@ class Board
 
   # maybe make a board with a array = ["|1|2|3|", "|4|5|6|", "|7|8|9|"]
   # then you access with array[0][0]
+    
+    # class variable, board
+    @@board_toe = ["|1|2|3|", "|4|5|6|", "|7|8|9|"]
 
+  def self.print_board()
+     
+    puts @@board_toe
+  end
 
+  ## update board with play
 end
 
 
@@ -19,3 +27,5 @@ class Play
 
 end
 
+
+Board.print_board()
