f44a89d316337850d97bed4d979b47ec544c7764
patorras <triotank@gmail.com>
2022-02-07 17:47:27 +0000

    start of game

---
+++ ruby/tic_tac_toe/main.rb
@@ -0,0 +1,21 @@
+# board design
+class Board
+
+  # maybe make a board with a array = ["|1|2|3|", "|4|5|6|", "|7|8|9|"]
+  # then you access with array[0][0]
+
+
+end
+
+
+# Player name, and score
+class Player
+  
+end
+
+
+# move, 
+class Play
+
+end
+
