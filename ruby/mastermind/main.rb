240a47b8a8350114735e77e09470314f27be5ca5
patorras <triotank@gmail.com>
2022-03-02 12:52:22 +0000

    at the moment i am able to let the computer random create the code, and let me guess it, next create a function for the computer guess my code

--- ruby/mastermind/main.rb
+++ ruby/mastermind/main.rb
@@ -29,12 +29,51 @@ starting with you guessing the computer’s random code.
 
 class Computer
 
+    def self.code_creation()
+        # just creates the random code if the computer is the code setter
+        random_array = Array.new(4) {rand 1..6}
+        return random_array
+    end
+
 end
 
 
 class Player
     
     # The code BREAKER will have 12 turns to decipher the code that the MAKER has created.
+
+    # is my guess == to computer code?
+
+    # if yes, i won
+
+    # elsif let give clues
+
+   
+
+    
+
+    def self.play(random_computer_array, my_guess)
+
+        for i in 0..3 do
+            # this would simple check if one of my guesses is in the code, do not provide feedback on position
+            if random_computer_array.include?(my_guess[i])
+                
+                # would compare if the position and number if correct
+                if random_computer_array[i] == my_guess[i] 
+                    puts "#{my_guess} #{my_guess[i]} exists in the code and is in the correct position" 
+                
+                # check if the number exists in the wrong position 
+                else
+                    puts "#{my_guess} #{my_guess[i]} exists in the code but is NOT in the correct position"
+                end
+            else
+            puts "#{my_guess} #{my_guess[i]} does not exist in the code"
+            end
+        end
+
+    
+    end
+
 end
 
 
@@ -77,6 +116,26 @@ class Code
             end
         end
     end
+
+    # method to give a guess, should return a array
+    def self.guessing_code()
+        guess_array = []
+
+        while guess_array.size < 4
+            puts "Please enter a digit for your guess"
+            guess_array << gets.chomp.to_i
+        end
+        return guess_array
+    end
+
+
+    def self.winner_check(code_setter, code_breaker)
+        if code_setter == code_breaker
+            puts "#{code_setter} is equal to #{code_breaker}"
+            return "Winner"
+        end
+    end
+
 end
 
 
@@ -95,9 +154,25 @@ until choice == "code" || choice == "breaker"
         Code.choosing_secret_code()
     elsif choice == "breaker"
         puts "You will never find out my secret code ahahahahahaha"
+        computer_secret_code = Computer.code_creation()
     else
         puts "I do not understand your choice, please insert code to set the code, or breaker to break the code"
     end
 end
 # You first choose to be either the code MAKER or code BREAKER.
 
+for i in 0..12
+     
+    player_guess_code = Code.guessing_code()
+    Player.play(computer_secret_code, player_guess_code)
+
+    if computer_secret_code == player_guess_code
+        puts "You have won the secret code was #{computer_secret_code}"
+        break
+    end
+      
+    
+
+end
+
+# now i need to loop the code above, for 12 plays.
