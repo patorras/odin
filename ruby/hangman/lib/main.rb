ed4814392162a240811635835c2821948fd8a462
patorras <triotank@gmail.com>
2022-04-06 11:52:19 +0100

    add classes for the board, game works, just need to add the letter that were choosen

--- ruby/hangman/lib/main.rb
+++ ruby/hangman/lib/main.rb
@@ -26,20 +26,72 @@ def new_secret_word()
 end
 #################################################################
 
+class Board
+
+  def self.create_empty_board(word)
+    empty_spaces = Array.new((word.length-1), "_")
+    return empty_spaces
+  end
+
+  def self.print_board(board)
+    board.each {|space| print space}
+    puts "\n"
+  end
+
+  def self.update_board(board, word, guess)
+
+    word.chars.each_with_index do |char, index|
+      if char == guess
+        board[index] = char
+      end
+    end
+
+    return board
+  end
+
+end
+
+
 secret_word = new_secret_word()
 
 # create the empty spaces for the letters and 
-empty_spaces = Array.new(secret_word.length, "_")
-
-# prints it
-empty_spaces.each {|space| print space}
-puts "\n"
+empty_spaces = Board.create_empty_board(secret_word)
 
 
+# prints board
+Board.print_board(empty_spaces)
 
+
+
+
+
+
+
+
+counter = 0
+
+while empty_spaces.include?("_") 
+
+  puts "Please choose a letter and press enter"
+  guess = gets.chomp
+
+
+
+  Board.update_board(empty_spaces, secret_word, guess)  
+  Board.print_board(empty_spaces)
+
+  counter += 1
+  if counter == 10
+    puts "You LOST"
+    break
+  end
+  
+end
 # convert secret word to array
-secret_word_array = secret_word.chars
-puts secret_word_array
+puts counter
+puts secret_word
+
+
 
 # count incorrect guesses they have before the game ends.
 
